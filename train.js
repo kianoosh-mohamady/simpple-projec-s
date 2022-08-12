26849f9d7803ce00669c2bd07ba4ce97e1626348
kianoosh-mohamady <111062681+kianoosh-mohamady@users.noreply.github.com>
2022-08-12 12:58:21 +0430

    Add files via upload

---
+++ train.js
@@ -0,0 +1,93 @@
+/*const kia = {};
+
+const notKia = {name:"tramp"};
+
+
+function isEmpty(obj){
+    for( const key in object){
+        if( i < 1 ){
+            return Error("your'e object is empty ");
+        }else{
+            return "your'e object is not empty ,goodluck(:";
+        }
+    }     for (const key in object) {
+        if (Object.hasOwnProperty.call(object, key)) {
+            const element = object[key];
+            
+        }
+    }   
+};
+
+
+
+const kian = {
+monthly: 3200,
+compensation: 400,
+inflationFactor: 100,
+children: 200,
+healthInsurance: -200,
+pensionInsurance: -250,
+unemploymentInsurance: -300,
+marriage: 125,
+};
+let result = " ";
+function grossSalary(kian){
+    result = monthly + compensation + inflationFactor + children + healthInsurance + pensionInsurance + unemploymentInsurance + marriage ;
+
+
+
+} ;
+//function calculate(kian){
+//    for(let i=0; i++ ; i+i){};
+//};
+const empty = isEmpty(Kia);
+
+
+
+console.log(empty);
+
+
+
+
+
+i am just going to kill myself(:
+
+*/
+/*const users = { name : "kianoosh",};
+
+
+function isEmpty(){
+    for (const key in users) {
+        if (Object.hasOwnProperty(key)) {
+            return "false"
+        }
+    }
+}
+const kia = isEmpty(users);
+document.write(kia);
+*/
+
+
+
+const user= {
+    monthly: 3200,
+    compensation: 400,
+    inflationFactor: 100,
+    children: 200,
+    healthInsurance: -200,
+    pensionInsurance: -250,
+    unemploymentInsurance: -300,
+    marriage: 125,
+    };
+   
+function grossSalary(obj){
+        for (const key in user) {
+            if (Object.hasOwnProperty.call(key, user)) {
+                const sum = " "
+               return sum = monthly + compensation + inflationFactor + children + healthInsurance + pensionInsurance + unemploymentInsurance + marriage;
+                
+            }
+        }
+    }
+const users = grossSalary(user);
+    document.write(users)
