c298dc397f17442615691dcc68cd13c61d6f9037
kianush <111062681+kianoosh-mohamady@users.noreply.github.com>
2022-08-16 11:54:12 +0430

    Update DoomsDay.js

--- DoomsDay.js
+++ DoomsDay.js
@@ -24,8 +24,44 @@ function displayTime(){
 };
 
 
-displayTime()
+displayTime();
 
 
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
 
+toggle.addEventListener('click', function () {
+    this.classList.toggle('bi-moon');
+    if (this.classList.toggle('bi-brightness-high-fill')) {
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2.5s';
+    } else {
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2.5s';
+    }
+});
+
+const arr =[
+    {
+        name:"earth",
+        job:"destroying the world",
+        id:0,
+    },
+    {
+        name:"sun",
+        job:"give life killer's",
+        id:1,
+    
+    },
+];
+
+function find(item, value){
+let res = " "
+ return resutl = res + item
+}
+
+
+console.log(find(arr[0].name))
 
